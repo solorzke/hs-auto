c249a1da5e5615e54aa5044da96c01a001ff1037
Kevin Solorzano <solorzke@gmail.com>
2020-07-27 17:28:32 -0400

    Navbar created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { MenuItems } from './Menuitems';
+import 'font-awesome/css/font-awesome.min.css';
+import { Button } from './Button';
+import './Navbar.css';
+
+export default class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			clicked: false
+		};
+	}
+
+	handleClick = () => {
+		this.setState({
+			clicked: !this.state.clicked
+		});
+	};
+
+	render() {
+		return (
+			<nav className="navbar-items">
+				<h1 className="navbar-logo">
+					React <i className="fa fa-bars" />
+				</h1>
+				<div className="menu-icon" onClick={this.handleClick}>
+					<i className={this.state.clicked ? 'fas fa-times' : 'fa fa-bars'} />
+				</div>
+				<ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+					{MenuItems.map((item, index) => {
+						return (
+							<li key={index}>
+								<a className={item.cName} href={item.url}>
+									{item.title}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+				<Button>SignUp</Button>
+			</nav>
+		);
+	}
+}
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import '../App.css';
+
+export default class HomePage extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+			</div>
+		);
+	}
+}
