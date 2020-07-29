52737ab8cd6613acaa9e76f1dc010476fb136fd5
Kevin Solorzano <solorzke@gmail.com>
2020-07-29 11:21:30 -0400

    Fixed web responsiveness on Statement component

--- src/components/MissionStatement/Statement.js
+++ src/components/MissionStatement/Statement.js
@@ -14,14 +14,18 @@ export default class Statement extends Component {
 						</div>
 					</div>
 					<div className="message-block">
-						<h1 style={{ padding: 10 }}>HS Auto Repair Shop</h1>
-						<h2 style={{ color: '#fe6847' }}>You're In Good Hands!</h2>
-						<p style={{ padding: 10 }}>
+						<h1 style={{ padding: 10 }} className="statement-title">
+							HS Auto Repair Shop
+						</h1>
+						<h3 style={{ color: '#fe6847' }} className="statement-slogan">
+							You're In Good Hands!
+						</h3>
+						<p style={{ padding: 10 }} className="statement-msg">
 							At HS Auto, you can relax knowing we will get the job done with anything that your vehicle
 							needs. We handle anything from oil changes, filter changes, battery replacements, auto body
 							upgrades, engine installations, car diagnostics, and more.
 						</p>
-						<p>Handled by expert and professional technicians.</p>
+						<p className="statement-msg">Handled by expert and professional technicians.</p>
 					</div>
 				</div>
 			</div>
