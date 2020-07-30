f1429e5f833c374d427f01283ee01beb1f9f92c4
Kevin Solorzano <solorzke@gmail.com>
2020-07-29 20:18:58 -0400

    Article Component created

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,14 +4,20 @@ import Slideshow from './Slideshow';
 
 export default class Banner extends Component {
 	render() {
+		const default_title = 'Welcome to HS Auto & Trucking Services';
+		const default_subtitle = 'We offer the best auto service in the Kearny area.';
 		return (
 			<div className="container">
 				<div className="banner-body">
 					<Slideshow />
 				</div>
 				<div className="banner-text">
-					<h1 className="banner-title">Welcome to HS Auto &amp; Trucking Services</h1>
-					<p className="banner-subtitle">We offer the best auto service in the Kearny area.</p>
+					<h1 className="banner-title">
+						{this.props.title === undefined ? default_title : this.props.title}
+					</h1>
+					<p className="banner-subtitle">
+						{this.props.subtitle === undefined ? default_subtitle : this.props.subtitle}
+					</p>
 				</div>
 			</div>
 		);
---
+++ src/views/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import Navbar from '../components/Navbar/Navbar';
+import Banner from '../components/Banner/Banner';
+import Article from '../components/Article/Article';
+import Footer from '../components/Footer/Footer';
+
+export default function About() {
+	return (
+		<div>
+			<Navbar />
+			<Banner title={'About Us'} subtitle={'Learn more about who we are at HS Auto & Trucking Services'} />
+			<Article />
+			<Footer />
+		</div>
+	);
+}
