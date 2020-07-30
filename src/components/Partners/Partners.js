1e4c20e8988b274c5749d411e636823e809111e8
Kevin Solorzano <solorzke@gmail.com>
2020-07-30 11:34:38 -0400

    About Us page created

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -10,18 +10,30 @@ export default function Partners() {
 		<div className="partners-container">
 			<h1 className="partners-heading">Our Partners</h1>
 			<div className="partners-row">
-				<a href="https://www.autozone.com/" target="_blank" rel="noopener">
-					<img src={AutoZone} height="100" width="300" />
-				</a>
-				<a href="https://shop.advanceautoparts.com/" target="_blank" rel="noopener">
-					<img src={AAP} height="100" width="400" />
-				</a>
-				<a href="https://partsauthority.com/" target="_blank" rel="noopener">
-					<img src={PartsAuthority} height="100" width="300" />
-				</a>
-				<a href="http://www.autopartsstoresusa.com/newjerseyautopartstores.htm" target="_blank" rel="noopener">
-					<img src={WilsonAuto} height="100" width="250" />
-				</a>
+				<div className="img-container">
+					<a href="https://www.autozone.com/" target="_blank" rel="noopener">
+						<img src={AutoZone} height="100" width="300" />
+					</a>
+				</div>
+				<div className="img-container">
+					<a href="https://shop.advanceautoparts.com/" target="_blank" rel="noopener">
+						<img src={AAP} height="100" width="400" />
+					</a>
+				</div>
+				<div className="img-container">
+					<a href="https://partsauthority.com/" target="_blank" rel="noopener">
+						<img src={PartsAuthority} height="100" width="300" />
+					</a>
+				</div>
+				<div className="img-container">
+					<a
+						href="http://www.autopartsstoresusa.com/newjerseyautopartstores.htm"
+						target="_blank"
+						rel="noopener"
+					>
+						<img src={WilsonAuto} height="100" width="250" />
+					</a>
+				</div>
 			</div>
 			<p className="partners-paragraph">
 				We are partnered with some of the best auto parts suppliers in the Garden State whose affordable prices
