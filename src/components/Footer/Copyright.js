953e720a861d5a6bd09f1945db9d53fa99809b73
Kevin Solorzano <solorzke@gmail.com>
2021-03-22 22:14:39 -0400

    Signature added

--- src/components/Footer/Copyright.js
+++ src/components/Footer/Copyright.js
@@ -4,7 +4,12 @@ import './Footer.css';
 export default function Copyright(props) {
 	return (
 		<div className="copyright-container">
-			<p>&copy; {props.year} HS Auto &amp; Trucking Services. All rights reserved </p>
+			<p>
+				&copy; {props.year} HS Auto &amp; Trucking Services. All rights reserved
+				<a href="https://www.solorzke.com/" className="signature" target="_blank">
+					Designed By: Solorzke Designs
+				</a>
+			</p>
 		</div>
 	);
 }
